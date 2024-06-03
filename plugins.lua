a9688914ece9c61ade6c047bf5b0b93405c24b08
Jarosław Zywert <j.zywert@piwik.pro>
2024-06-03 10:08:34 +0200

    venv selector branch change

--- plugins.lua
+++ plugins.lua
@@ -105,6 +105,7 @@ local plugins = {
   },
   {
     "linux-cultist/venv-selector.nvim",
+    branch = "regexp",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     config = function()
       require("venv-selector").setup { name = { "venv", ".venv" }, auto_refresh = true }
