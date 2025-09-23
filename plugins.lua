98989fffdf6679be720b027e03e1a5addf7ae895
Jarosław Zywert <j.zywert@piwik.pro>
2025-09-23 07:46:11 +0200

    deprecation messages fixed

--- plugins.lua
+++ plugins.lua
@@ -106,7 +106,7 @@ local plugins = {
   },
   {
     "linux-cultist/venv-selector.nvim",
-    branch = "regexp",
+    branch = "main",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     config = function()
       require("venv-selector").setup { name = { "venv", ".venv" }, auto_refresh = true }
